3b0fbf3110cd0d5713df003a78aa8dd5ff69c1da
Alisson Matias <alissonmds00@gmail.com>
2022-07-28 09:13:07 -0300

    fix: using html extension instead EJ S

---
+++ api.js
@@ -0,0 +1,5 @@
+export const API_KEY = 'c971480b4edf2658535b9f7e359cb43e';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const language = 'language=pt-BR';
+
