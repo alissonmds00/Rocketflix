536c18ecf9d58a9324f8e86982488f012fbaa4cf
Alisson Matias <alissonmds00@gmail.com>
2022-07-28 09:40:08 -0300

    feat: starting the function

--- api.js
+++ api.js
@@ -1,5 +1,14 @@
-export const API_KEY = 'c971480b4edf2658535b9f7e359cb43e';
-export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
-export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-export const language = 'language=pt-BR';
+const ApiKey = 'c971480b4edf2658535b9f7e359cb43e';
+const BaseUrl = 'https://api.themoviedb.org/3/movie/';
+const ImgUrl = 'https://image.tmdb.org/t/p/w500';
+const Language = 'language=pt-BR';
 
+const buttom = document.getElementById('shuffle')
+const nome = document.getElementById('nome')
+const desc = document.getElementById('desc')
+const synopsis = document.getElementById('sinopse')
+
+function shuffle() {
+    
+}
+buttom.addEventListener('click', shuffle)
--- main.js
+++
@@ -1,5 +0,0 @@
-import {
-  API_KEY, BASE_URL,
-  IMG_URL,
-  language,
-} from './api.js'
