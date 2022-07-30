9d87b988f54e72e22606f8b65b45c1002d9ba12c
Alisson Matias <alissonmds00@gmail.com>
2022-07-29 21:55:32 -0300

    feat: api now running correctly

--- api.js
+++ api.js
@@ -1,13 +1,13 @@
 /*                    API CONTENT                  */
 const ApiKey = 'c971480b4edf2658535b9f7e359cb43e';
 const BaseUrl = 'https://api.themoviedb.org/3/movie/';
-const ImgUrl = 'https://image.tmdb.org/t/p/w';
+const ImgUrl = 'https://image.tmdb.org/t/p/w500';
 const Language = 'language=pt-BR';
 /* ----------------------------------------------- */
 
 /*                  Doom  items                     */
 const buttom = document.getElementById('shuffle')
-const content = document.getElementById('main').innerHTML
+const content = document.getElementById('main')
 const name = document.getElementById('nome').innerHTML
 const desc = document.getElementById('desc').innerHTML
 const synopsis = document.getElementById('sinopse').innerHTML
@@ -16,23 +16,26 @@ const thumb = document.getElementById('thumb').innerHTML
 
 
 function shuffle() {
-    const random = Math.floor(Math.random() * 10000)
-    const urlDesc = `${BaseUrl}${random}?api_key=${ApiKey}&${Language}`
-    let path, overview
+    document.getElementById('main').removeAttribute('style')
+    let random = Math.floor(Math.random() * 9999)
+    let urlDesc = `${BaseUrl}${random}?api_key=${ApiKey}&${Language}`
     function getData() {
         axios.get(urlDesc)
             .then(response => {
                 let Data = response.data
-                path = JSON.stringify(Data.poster_path)
+                let path = JSON.stringify(Data.poster_path)
+                path = path.replace('"', "")
+                path = path.replace('"', "")
                 document.getElementById('nome').innerHTML = JSON.stringify(Data.title)
-                document.getElementById('sinopse').innerHTML = JSON.stringify(Data.overview)
+                document.getElementById('desc').innerHTML = JSON.stringify(Data.overview)
+                let urlImg = `${ImgUrl}${path}` 
+                document.getElementById('thumb').setAttribute('src', urlImg)
                 console.log(Data)
+                console.log(path)
+                console.log(ImgUrl)
             })
             .catch(error => console.log(error))
     }
-
-    const urlImg = `${ImgUrl}${random}.${path}`
-
     getData()
 }
 
